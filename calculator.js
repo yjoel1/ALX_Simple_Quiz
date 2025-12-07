b02a8c35495e1e40a6ca6bbd1115ec0808361d1f
yjoel1 <joelyeddu@gmail.com>
2025-12-07 05:15:20 -0800

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,21 @@
+// Arithmetic Functions
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    if (number2 === 0) {
+        return "Error: Division by zero";
+    }
+    return number1 / number2;
+}
+
+// Helper: Get numeric values from input fields
